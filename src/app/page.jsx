9d494f7ce26008c8ecc2624a3c6c5f7eb324b6b3
finypres <finypres@proton.me>
2023-07-25 17:54:28 -0500

    v0.1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Script from 'next/script'
 import Main from './components/template/main/Main'
-// import ScriptLoader from './components/script_loader/ScriptLoader'
+import ScriptLoader from './components/script_loader/ScriptLoader'
 
 export default function Home() {
   return (
@@ -8,8 +8,8 @@ export default function Home() {
       <Main />
       <Script src='/js/plugins/js/plugins.min.js' />
       <Script src='/js/theme.js' />
-      {/* <ScriptLoader src='/js/theme.js' />
-      <ScriptLoader src='/js/theme.init.js' /> */}
+      <ScriptLoader src='/js/theme.js' />
+      <ScriptLoader src='/js/theme.init.js' />
     </>
   )
 }
