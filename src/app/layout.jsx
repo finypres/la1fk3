584a4ee873eef05c138064adeb523d112563245c
finypres <finypres@proton.me>
2023-08-15 09:32:03 -0500

    [FIX]

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,11 @@
+import Script from 'next/script'
 import 'react-toastify/dist/ReactToastify.min.css'
-import {
-  GoogleTagManager,
-  GoogleTagManagerNoScript
-} from './components/google-tags/GoogleTags'
 import Notice from './components/notice/Notice'
 import Footer from './components/template/footer/Footer'
 import Header from './components/template/header/Header'
 import WhatsAppButton from './components/whatsapp/WhatsAppButton'
 import './globals.css'
+const GTM_ID = process.env.NEXT_PUBLIC_TM_TRACKING_ID
 
 export const metadata = {
   title: 'Finanzas y Préstamos | Hacemos realidad tus metas financieras',
@@ -18,9 +16,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <GoogleTagManager
-        TM_TRACKING_ID={process.env.NEXT_PUBLIC_TM_TRACKING_ID}
-      />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body>
         <div className="alternative-font-7">
           <div className="body mt-0">
@@ -31,8 +35,10 @@ export default function RootLayout({ children }) {
             <WhatsAppButton />
           </div>
         </div>
-        <GoogleTagManagerNoScript
-          TM_TRACKING_ID={process.env.NEXT_PUBLIC_TM_TRACKING_ID}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`
+          }}
         />
       </body>
     </html>
