2f446a690298365d06a9a2b483fe49659cc8ac64
finypres <finypres@proton.me>
2023-08-14 21:33:39 -0500

    [FIX]

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,9 +5,8 @@ import Header from './components/template/header/Header'
 import './globals.css'
 import WhatsAppButton from './components/whatsapp/WhatsAppButton'
 import {
-  GTM
-  // GoogleAnalytics,
-  // GoogleLinkConversion
+  GoogleAnalytics,
+  GoogleLinkConversion
 } from './components/google-tags/GoogleTags'
 
 export const metadata = {
@@ -19,16 +18,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <GTM
-        TM_TRACKING_ID={process.env.NEXT_PUBLIC_TM_TRACKING_ID}
-      />
-      {/* <GoogleAnalytics
+      <GoogleAnalytics
         AW_TRACKING_ID={process.env.NEXT_PUBLIC_AW_TRACKING_ID}
       />
       <GoogleLinkConversion
         AW_TRACKING_ID={process.env.NEXT_PUBLIC_AW_TRACKING_ID}
         AW_LINK_CONVERSION_ID={process.env.NEXT_PUBLIC_AW_LINK_WA_ID}
-      /> */}
+      />
       <body>
         <div className="alternative-font-7">
           <div className="body mt-0">
